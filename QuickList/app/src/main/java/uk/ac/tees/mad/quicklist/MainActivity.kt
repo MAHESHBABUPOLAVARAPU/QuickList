2108db1faabcc459fcf390418846628b5fb5e8d9
MAHESHBABUPOLAVARAPU <s3522812@live.tees.ac.uk>
2025-11-10 14:02:18 +0000

    fifth change

--- QuickList/app/src/main/java/uk/ac/tees/mad/quicklist/MainActivity.kt
+++ QuickList/app/src/main/java/uk/ac/tees/mad/quicklist/MainActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import uk.ac.tees.mad.quicklist.screens.AuthScreen
 import uk.ac.tees.mad.quicklist.screens.SplashScreen
 import uk.ac.tees.mad.quicklist.ui.theme.QuickListTheme
@@ -17,8 +21,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuickListTheme {
-                AuthScreen()
+                NavigationGraph()
             }
+        }
+    }
+}
+
+
+sealed class NavigateItems(val routes : String) {
+    object Splash : NavigateItems("splash")
+    object Auth : NavigateItems ( "auth")
+}
+
+
+@Composable
+fun NavigationGraph(){
+    val navController = rememberNavController()
+    NavHost(navController, NavigateItems.Splash.routes){
+        composable(NavigateItems.Splash.routes){
+            SplashScreen()
+        }
+        composable(NavigateItems.Auth.routes){
+            AuthScreen()
         }
     }
 }
