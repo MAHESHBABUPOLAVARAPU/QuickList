0c1311f7bb253b16d3c4f367551c41a3b013e02e
MAHESHBABUPOLAVARAPU <s3522812@live.tees.ac.uk>
2025-11-10 13:52:50 +0000

    second change

--- QuickList/app/src/main/java/uk/ac/tees/mad/quicklist/MainActivity.kt
+++ QuickList/app/src/main/java/uk/ac/tees/mad/quicklist/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import uk.ac.tees.mad.quicklist.screens.SplashScreen
 import uk.ac.tees.mad.quicklist.ui.theme.QuickListTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +16,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuickListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                }
+                SplashScreen()
             }
         }
     }
