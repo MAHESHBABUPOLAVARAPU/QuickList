7305c401f602d1345842a81b95b8a87542306b97
MAHESHBABUPOLAVARAPU <s3522812@live.tees.ac.uk>
2025-11-20 15:29:13 +0000

    delete task added

--- QuickList/app/src/main/java/uk/ac/tees/mad/quicklist/presentation/Screens/HomeScreen.kt
+++ QuickList/app/src/main/java/uk/ac/tees/mad/quicklist/presentation/Screens/HomeScreen.kt
@@ -1,7 +1,13 @@
 package uk.ac.tees.mad.quicklist.presentation.Screens
 
+import android.Manifest
+import android.R.attr.description
+import android.R.id.message
+import android.app.Activity
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,8 +16,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -20,12 +28,15 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.RadioButtonUnchecked
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -39,15 +50,23 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import uk.ac.tees.mad.quicklist.presentation.ViewModel.GetTask
 import uk.ac.tees.mad.quicklist.presentation.ViewModel.HomeViewModel
 import uk.ac.tees.mad.safeher.presentation.Screens.utilScreens.AddTaskDialog
 import uk.ac.tees.mad.safeher.presentation.ViewModel.AuthViewModel
+import kotlin.coroutines.coroutineContext
 
 @Composable
 fun HomeScreen(
