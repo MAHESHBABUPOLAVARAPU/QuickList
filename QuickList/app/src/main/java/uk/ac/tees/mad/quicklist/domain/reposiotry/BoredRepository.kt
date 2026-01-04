ae0e83b5c01b0b0bc3facfb917cb3b68f797dd05
MAHESHBABUPOLAVARAPU <s3522812@live.tees.ac.uk>
2026-01-04 11:09:12 +0000

    Vibration and notification implemented and finalised

--- QuickList/app/src/main/java/uk/ac/tees/mad/quicklist/domain/reposiotry/BoredRepository.kt
+++
@@ -1,9 +0,0 @@
-package uk.ac.tees.mad.quicklist.domain.reposiotry
-
-import uk.ac.tees.mad.quicklist.data.remote.api.activityDto.ActivityDtoItem
-
-interface BoredRepository  {
-
-    suspend fun getRandomActivity(type: String) : List<ActivityDtoItem>
-
-}
